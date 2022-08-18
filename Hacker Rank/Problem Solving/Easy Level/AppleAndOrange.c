a06b9cd1db0a2df89de3683cffa5d17b92187c08
Ahmed Mohammed Hussanein <engineer.ahmed1812@gmail.com>
2022-08-18 21:27:04 +0200

    Updating

--- Hacker Rank/Problem Solving/Easy Level/AppleAndOrange.c
+++ Hacker Rank/Problem Solving/Easy Level/AppleAndOrange.c
@@ -4,6 +4,17 @@
 	Engineer: Ahmed Mohammed Hussanein Ali.
  	*/
 
+/*
+	Proplem Statement:
+	The red region denotes the house, where s is the start point, and t is the endpoint.
+	The apple tree is to the left of the house, and the orange tree is to its right.
+	Assume the trees are located on a single point, where the apple tree is at point a, and the orange tree is at point b.
+	When a fruit falls from its tree, it lands d units of distance from its tree of origin along the x-axis.
+	*A negative value of d means the fruit fell d units to the tree's left, and a positive value of d means it falls d units to the tree's right. *
+	Given the value of d for m apples and n oranges, determine how many apples and oranges will fall on Sam's house (i.e., in the inclusive range )?
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
